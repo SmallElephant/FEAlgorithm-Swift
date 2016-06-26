10ef2b9090b5dcdc3d94625c42cde8a0e37b407d
FlyElephant <1061731917@qq.com>
2016-06-26 16:37:20 +0800

    Hext Coversion~

--- 5-Tree/5-Tree/main.swift
+++ 5-Tree/5-Tree/main.swift
@@ -8,61 +8,6 @@
 
 import Foundation
 
-
-
-func preOrder(rootNode:TreeNode?) -> Void {
-    if rootNode != nil {
-        if let data = rootNode?.data {
-            print("\(data)\t", terminator: "")
-            preOrder(rootNode?.leftChild)
-            preOrder(rootNode?.rightChild)
-        }
-    }
-}
-
-func inOrder(rootNode:TreeNode?) -> Void {
-    if rootNode != nil {
-        if let data = rootNode?.data {
-            inOrder(rootNode?.leftChild)
-            print("\(data)\t", terminator: "")
-            inOrder(rootNode?.rightChild)
-        }
-    }
-}
-
-func postOrder(rootNode:TreeNode?) -> Void {
-    if rootNode != nil {
-        if let data = rootNode?.data {
-            postOrder(rootNode?.leftChild)
-            postOrder(rootNode?.rightChild)
-            print("\(data)\t", terminator: "")
-        }
-    }
-}
-
-func levelOrder(rootNode:TreeNode?) -> Void {
-    var arr:[AnyObject]=[];
-    arr.append(rootNode!);
-    
-    while arr.count>0 {
-        let firstNode=arr[0] as! TreeNode
-        
-        if let data=firstNode.data {
-            print("\(data)\t", terminator: "")
-            arr.removeFirst()
-        }
-        
-        if (firstNode.leftChild != nil) {
-            arr.append(firstNode.leftChild!)
-        }
-        
-        if (firstNode.rightChild != nil) {
-            arr.append(firstNode.rightChild!)
-        }
-    }
-}
-
-
 var d = TreeNode(data: "d",leftChild: nil,rightChild: nil)
 
 var b = TreeNode(data: "b",leftChild: nil,rightChild: d)
@@ -119,22 +64,60 @@ func createTree(inout root:TreeNode?) -> Void {
 //print("\n层次遍历:")
 //levelOrder(rootNode)
 //print()
+//
+//var constructTree = ConstructBinaryTree()
+//var rePreOrder:[String] = ["1","2","4","7","3","5","6","8"]
+//var reInOrder:[String] = ["4","7","2","1","5","3","8","6"]
+//var rePostOrder:[String] = ["7","4","2","5","8","6","3","1"]
+//var constructRoot = constructTree.reConstructForPost(rePreOrder, inOrder: reInOrder)
+//print("FlyELephant")
+//print("先序和中序输出后序:")
+//postOrder(constructRoot)
+//
+//var preRoot = constructTree.reConstructForPre(reInOrder, postOrder: rePostOrder)
+//print("\n中序和后序输出先序:")
+//preOrder(preRoot)
+//print("")
 
-var constructTree = ConstructBinaryTree()
-var rePreOrder:[String] = ["1","2","4","7","3","5","6","8"]
-var reInOrder:[String] = ["4","7","2","1","5","3","8","6"]
-var rePostOrder:[String] = ["7","4","2","5","8","6","3","1"]
-var constructRoot = constructTree.reConstructForPost(rePreOrder, inOrder: reInOrder)
-print("FlyELephant")
-print("先序和中序输出后序:")
-postOrder(constructRoot)
+func binaryPosition(position:NSInteger)->NSInteger {
+    var result=1
+    for _  in 0..<position {
+        result = result*2
+    }
+    return result
+}
 
-var preRoot = constructTree.reConstructForPre(reInOrder, postOrder: rePostOrder)
-print("\n中序和后序输出先序:")
-preOrder(preRoot)
-print("")
+func convertBinaryToDecimal(binaryNumber:String)->NSInteger {
+    var result=0
+    var temp:NSInteger
+    var power:NSInteger
+    for index in 0..<binaryNumber.characters.count {
+        temp = Int(binaryNumber[index] as String)!
+        power = binaryNumber.characters.count-1-index
+        result = result+temp*binaryPosition(power)
+    }
+    return result
+}
 
+func convertNumberToHex(number:NSInteger)->String{
+    if number<=0 {
+        return "0"
+    }
+    var result:String = ""
+    var num = number
+    while(num != 0){
+        result=String(num%2)+result
+        num=num/2
+    }
+    return result
+}
 
+var binaryNumber="1111"
+var resultValue=convertBinaryToDecimal(binaryNumber)
+print("\(binaryNumber)的值:\(resultValue)")
+
+var binaryHex=convertNumberToHex(resultValue)
+print("FlyElephant:\(binaryHex)")
 
 
 
