72cc967f74e3f4fdc26b5cf52445557c08225cf1
FlyElephant <keso_xiang@talkmate.com>
2017-02-20 11:50:51 +0800

    背包和完全背包解决~

--- 2-Dynamic/2-Dynamic/Bag.swift
+++ 2-Dynamic/2-Dynamic/Bag.swift
@@ -14,10 +14,11 @@ class Bag {
     var weights:[Int] = []
     var maxWeight:Int = 0
     var calculate:[[Int]] = []
+    var calculate2:[Int] = []
+    var calculate3:[Int] = []
     
-    
-    init(max:Int,value:[Int],weight:[Int]) {
-        maxWeight = max
+    init(maxW:Int,value:[Int],weight:[Int]) {
+        maxWeight = maxW
         values = value
         weights = weight
         let temp:[Int] = [Int].init(repeating: 0, count: maxWeight + 1)
@@ -29,15 +30,57 @@ class Bag {
         for i in 1..<values.count {
             for j in 1...maxWeight {
                 if weights[i] <= j {
-                    let currentValue:Int = values[i] + calculate[i - 1][j - weights[i]]
-                    let preValue:Int = calculate[i-1][j]
-                    calculate[i][j] = currentValue > preValue ? currentValue : preValue;
+                    calculate[i][j] = maxCompare(a: values[i] + calculate[i - 1][j - weights[i]], b: calculate[i-1][j])
                 } else {
                     calculate[i][j] = calculate[i - 1][j]
                 }
             }
         }
         return calculate[values.count - 1][maxWeight]
+    }
+    
+    func solveMaxValue2()->Int {
+        calculate2 = [Int].init(repeating: 0, count: maxWeight + 1)
+        
+        for i in 1..<values.count {
+            for j in (1...maxWeight).reversed() {
+                if weights[i] <= j {
+                    calculate2[j] = maxCompare(a:  calculate2[j], b:  (calculate2[j-weights[i]]+values[i]))
+                }
+            }
+        }
+        return calculate2[maxWeight]
+    }
+    
+    func solveMaxValue3()->Int {
+        calculate3 = [Int].init(repeating: 0, count: maxWeight + 1)
+        
+        for i in 1..<values.count {
+            for j in 1...maxWeight {
+                if weights[i] <= j {
+                    calculate3[j] = maxCompare(a:  calculate3[j], b:  (calculate3[j-weights[i]]+values[i]))
+                }
+            }
+        }
+        return calculate3[maxWeight]
+    }
+    
+    func printResult() {
+        for i in 1..<values.count {
+            for j in 1...maxWeight {
+                print("\(i)--\(j)的结果---\(calculate[i][j])")
+            }
+        }
+    }
+    
+    func printResult2() {
+        for j in 1...maxWeight {
+            print("\(j)的结果---\(calculate2[j])")
+        }
+    }
+    
+    func maxCompare(a:Int,b:Int) -> Int {
+        return a > b ? a : b
     }
     
 }
--- 2-Dynamic/2-Dynamic/main.swift
+++ 2-Dynamic/2-Dynamic/main.swift
@@ -8,9 +8,27 @@
 
 import Foundation
 
-var bag:Bag = Bag.init(max: 50, value:  [0, 60, 100, 120], weight: [0, 10, 20, 30])
+var values:[Int] = [0, 60, 100, 120]
+
+var weights:[Int] = [0, 10, 20, 30]
+
+values = [0,6,3,5,4,6]
+
+weights = [0,2,2,6,5,4]
+
+var max:Int = 10
+
+var bag:Bag = Bag.init(maxW: max, value: values , weight: weights)
 
 var maxValue = bag.solveMaxValue()
 
 print("最大的值----\(maxValue)")
 
+bag.printResult()
+
+var maxValue2 = bag.solveMaxValue2()
+print("最大值为---\(maxValue2)")
+bag.printResult2()
+
+var maxValue3 = bag.solveMaxValue3()
+print("最大值为---\(maxValue3)")
